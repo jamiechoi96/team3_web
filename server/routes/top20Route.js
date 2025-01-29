ca2d6e41a7154696bfbd51552847c4f360aaca5b
jamiechoi96 <jamiechoi96@naver.com>
2025-01-29 18:32:13 +0900

    api 통합으로 인한 삭제

--- server/routes/top20Route.js
+++
@@ -1,8 +0,0 @@
-// This file is renamed to top20Route.js
-const express = require('express');
-const router = express.Router();
-const { getTop20 } = require('../controllers/top20Controller');
-
-router.get('/top20', getTop20);
-
-module.exports = router;
