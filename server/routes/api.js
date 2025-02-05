9160e69ae7463cbd66d20c90f31544b178d58131
jamiechoi96 <jamiechoi96@gmail.com>
2025-02-05 17:50:36 +0900

    api 수정

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const { getWatchHistory } = require('../controllers/watchHistoryController');
 const { getTop20 } = require('../controllers/top20Controller');
+const authController = require('../controllers/authController');
 
 // 테스트
 router.get('/test', (req, res) => {
@@ -14,5 +15,13 @@ router.get('/watch-history', getWatchHistory);
 
 // TOP 20 영화 순위 조회
 router.get('/top20', getTop20);
+
+// 로그인 라우트
+router.post('/auth/login', authController.login);
+
+// 보호된 라우트 예시
+router.get('/auth/protected', authController.authenticateToken, (req, res) => {
+    res.json({ message: '인증된 사용자입니다.', user: req.user });
+});
 
 module.exports = router;
