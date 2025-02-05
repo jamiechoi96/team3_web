c599108c31d384f56b6f5f37ce75c95ea89cbc82
jamiechoi96 <jamiechoi96@gmail.com>
2025-02-05 17:12:16 +0900

    로그인 페이지구현으로 인한 메인 페이지 변화

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import {
   Routes,
   Route,
   useLocation,
+  Navigate,
 } from "react-router-dom"; // React Router 추가
 import Header from "./components/Header/Header.jsx";
 import Home from "./components/Main/Home/Home.jsx";
-import MyPage from "./components/Mypage/MyPage.jsx"; // MyPage 컴포넌트 임포트
 import Search from "./components/Search/Search.jsx"; // Search 컴포넌트 임포트
 import Footer from "./components/Footer/Footer.jsx";
+import Login from "./components/Login/Login.jsx";
+import MyPage from "./components/MyPage/MyPage.jsx"; // MyPage 컴포넌트 임포트
 import "./App.css";
 
 // 동적으로 클래스 추가를 위한 컴포넌트
@@ -16,12 +18,18 @@ function Layout() {
   const location = useLocation(); // 현재 경로 확인
   const isHome = location.pathname === "/"; // Home 페이지 여부 확인
 
+  // 로그인 상태 확인
+  const isLoggedIn = localStorage.getItem('token') !== null;
+
+  // 로그인되지 않은 경우 로그인 페이지로 리다이렉트
+  if (!isLoggedIn) {
+    return <Navigate to="/login" />;
+  }
+
   return (
     <>
       <Header />
       <div className={`main ${isHome ? "home-page" : ""}`}>
-        {" "}
-        {/* Home 페이지일 때만 추가 클래스 적용 */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/movies" element={<Home />} />
@@ -37,7 +45,10 @@ function Layout() {
 function App() {
   return (
     <Router>
-      <Layout />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<Layout />} />
+      </Routes>
     </Router>
   );
 }
